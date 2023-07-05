7381581193d42b50f6fed413368e3c586e9b33ca
dgarcia <manuonda@gmail.com>
2023-07-04 21:44:23 -0300

    files project expo and folder crud

---
+++ navigation-example/app/_layout.tsx
@@ -0,0 +1,45 @@
+import {Stack, Tabs, useRouter} from 'expo-router'
+import {AntDesign} from '@expo/vector-icons'
+import {Alert} from 'react-native'
+
+export default Tabs;
+
+/*
+export default function Layout(){
+    const router = useRouter();
+    return(
+       <Stack 
+         screenOptions={{
+            title:'Title',
+            headerStyle:{ 
+                 backgroundColor: '#FFE030'
+            },
+            headerTintColor: '#1E2632',
+            headerTitleStyle :{
+              fontWeight: 'bold'
+            }
+            
+         }}
+       >
+       <Stack.Screen name="index" options={{
+         title: 'Home',
+         headerRight: () => (
+                <AntDesign
+                 onPress={() => router.push('/modal')}
+                 name="infocirlceo"  size={24}
+                 color="black"/>
+            )
+        }}/>
+       <Stack.Screen name="profile" options={{title:'Profile'}} />
+       <Stack.Screen name="modal" options={{
+           title:'Modal' , 
+           presentation:'modal',
+           headerStyle: {
+             backgroundColor:'white'
+           },
+           headerTintColor: '#1E2632'
+           }} />
+      </Stack>
+    )
+    
+}*/
